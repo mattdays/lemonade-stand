a4def143ffe3e10e60eda4cb4e9785c74921b2a9
Brandon Ra <brandon.ra97@gmail.com>
2019-02-26 20:47:52 -0500

    Update StartBot.java

--- StartBot.java
+++ StartBot.java
@@ -11,13 +11,27 @@ import java.util.Random;
  */
 public class StartBot implements Bot {
   private Random generator = new Random();
+  private ArrayList<ArrayList<Integer>> history = new ArrayList<ArrayList<Intger>>();
   private List<Integer> historyP0 = new ArrayList<>();
   private List<Integer> historyP1 = new ArrayList<>();
   private List<Integer> historyP2 = new ArrayList<>();
+  history.add(historyP0);
+  history.add(historyP1);
+  history.add(historyP2);
   private Integer roundNum = 1;
-  private Float responseR = (float) 0.75;
-  private float tol = (float) 0.1;
-  private float pScale = (float) 0.5;
+  private double responseR = 0.75;
+  private double tol = 0.1;
+  private double pScale = 0.5;
+
+  private int oppSide(int player){
+    if(player <= 6){
+      return player + 6;
+    }
+    else{
+      return player - 6;
+    }
+    // return (player + 6) % 12
+  }
 
   private int minDist(int player1, int player2){
     int smaller = 0;
@@ -34,48 +48,57 @@ public class StartBot implements Bot {
     return Math.min(dist2, dist1);
   }
 
-  private float sumConst(){
-    float sum = 0;
+  private double sumConst(){
+    double sum = 0;
     for(int k = 2; k < roundNum; k++) {
       sum += Math.pow(responseR, roundNum - 1 - k);
     }
     return sum;
   }
 
-  private float stickIndex(float sumConst, int player){
-    float sum = 0;
+  private double stickIndex(float sumConst, int player){
+    double sum = 0;
     // Must do if check for which player to compare against
-    if(player == 0){
-      minD = minDist(historyP0[k], historyP0[k-1]);
-      minD = Math.pow(minD, pScale);
-    }
-    else if(player == 1){
-      minD = minDist(historyP0[k], historyP0[k-1]);
-      minD = Math.pow(minD, pScale);
-    }
-    else{
-      minD = minDist(historyP1[k], historyP2[k-1]);
-      minD = Math.pow(minD, pScale);
-    }
     for(int k = 2; k < roundNum; k++){
-      sum += (Math.pow(responseR, roundNum - 1 - k) / sumConst) * minDist(historyP1[k], historyP1[]);
+      sum += (Math.pow(responseR, roundNum - 1 - k) / sumConst) *
+                Math.pow((minDist(history.get(player).get(k),
+                  history.get(player).get(k - 1)), pScale);
     }
-    return (float) 0.0;
+    return sum;
+  }
+
+  private double followPair(float sumConst, int player1, int player2){
+    double sum = 0;
+    for(int k = 2; k < roundNum; k++){
+      int opp = oppSide(history.get(player2).get(k - 1));
+      sum += (Math.pow(responseR, roundNum - 1 - k) / sumConst) *
+              Math.pow((minDist(history.get(player1).get(k), opp), pScale);
+    }
+    return sum;
 
   }
 
-  private float followIndex(float sumConst, int player){
-        // Must do if check for which player to compare against
-
-
-    return (float) 0.0;
-
-  }
-
-  private float followPair(float sumConst){
-        // Must do if check for which player to compare against
-
-    return (float) 0.0;
+  private double followIndex(int player){
+    int opp2, opp3, curr, minD;
+    double sum = 0;
+    int player2 = (player + 1) % 3;
+    int player3 = (player + 2) % 3;
+    for(int k = 2; k < roundNum; k++){
+      opp2 = oppSide(history.get(player2).get(k - 1));
+      opp3 = oppSide(history.get(player3).get(k - 1));
+      curr = history.get(player).get(k);
+      minD1 = minDist(curr, opp2);
+      minD2 = minDist(curr, opp3);
+      if(minD1 < minD2){
+        minD = minD1;
+      }
+      else{
+        minD = minD2;
+      }
+      sum += (Math.pow(responseR, roundNum - 1 - k) / sumConst) *
+                Math.pow(minD, pScale);
+    }
+    return sum;
   }
 
 
@@ -97,7 +120,8 @@ public class StartBot implements Bot {
     }
 
     int nextMove = generator.nextInt(12) + 1;
-    historyP0.add(nextMove);
+    history.get(0).add(nextMove);
+    // historyP0.add(nextMove);
     return nextMove;
   }
 
@@ -113,10 +137,12 @@ public class StartBot implements Bot {
    * @return the next action to play.
    */
   public void recordHistory(int player1LastMove, int player2LastMove) {
-    historyP1.add((player1LastMove));
-    historyP2.add((player2LastMove));
-    System.out.println("h1" + historyP1);
-    System.out.println("h2" + historyP2);
+    history.get(1).add(player1LastMove);
+    history.get(2).add(player2LastMove);
+    // historyP1.add((player1LastMove));
+    // historyP2.add((player2LastMove));
+    System.out.println("h1" + history.get(1));
+    System.out.println("h2" + history.get(2));
   }
 
   public static void main(String[] args) {
@@ -124,4 +150,3 @@ public class StartBot implements Bot {
     System.out.println("test");
   }
 }
-

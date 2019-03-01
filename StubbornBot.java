987aa970cc1c654522c9c3770244c79c8576c7e1
Matthew <daysmatthew@gmail.com>
2019-02-28 19:26:36 -0500

    Added utility history tracker

--- StubbornBot.java
+++ StubbornBot.java
@@ -2,14 +2,18 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
-/** A Lemonade Stand game player that is extremely attached to the idea of being difficult.
-  * 
-  * @author MD, BR
-  */
+
+/**
+ * A Lemonade Stand game player that is extremely attached to the idea of being
+ * difficult.
+ * 
+ * @author MD, BR
+ */
 public class StubbornBot implements Bot {
     private List<Integer> historyP0 = new ArrayList<>();
     private List<Integer> historyP1 = new ArrayList<>();
     private List<Integer> historyP2 = new ArrayList<>();
+    private int totalUtility = 0;
     private int roundCount = 0;
     private Random generator = new Random();
 
@@ -22,8 +26,7 @@ public class StubbornBot implements Bot {
             return 8; // three-way tie
         else if ((action1 == action2) || (action1 == action3)) {
             return 6; // two-way tie
-        }
-        else {
+        } else {
             int score = 0;
             int i = action1;
             while ((i != action2) && (i != action3)) { // score clockwise
@@ -32,13 +35,12 @@ public class StubbornBot implements Bot {
             }
             i = action1;
             while ((i != action2) && (i != action3)) { // score anti-clockwise
-                i = (i-1 > 0) ? i-1 : 12;
+                i = (i - 1 > 0) ? i - 1 : 12;
                 score += 1;
             }
             return score;
         }
-    } 
-
+    }
 
     public int getNextMove(int player1LastMove, int player2LastMove) {
         int utility = -1;
@@ -46,26 +48,29 @@ public class StubbornBot implements Bot {
         // , utility, newPos;
 
         if (this.roundCount > 0) {
-          //Check utility based on previous
-          utility = scoreRound(this.historyP0.get(this.historyP0.size() - 1), this.historyP1.get(this.historyP1.size() - 1), this.historyP2.get(this.historyP2.size() - 1));
-          if (utility < 8) {
-            newPos = (this.historyP0.get(this.historyP0.size() - 1) + 4) % 12;
-            if (newPos == 0) {
-                newPos = 12;
+            // Check utility based on previous
+            utility = scoreRound(this.historyP0.get(this.historyP0.size() - 1),
+                    this.historyP1.get(this.historyP1.size() - 1), this.historyP2.get(this.historyP2.size() - 1));
+            // this.utilityHistory.add(utility);
+            totalUtility += utility;
+            float avgUtility = ((float)this.totalUtility) / roundCount;
+            if (avgUtility < 8) {
+                newPos = (this.historyP0.get(this.historyP0.size() - 1) + 4) % 12;
+                if (newPos == 0) {
+                    newPos = 12;
+                }
+            } else {
+                newPos = this.historyP0.get(this.historyP0.size() - 1);
             }
-          }
-          else{
-              newPos = this.historyP0.get(this.historyP0.size() - 1);
-          }
+        } else {
+            newPos = randomizePosition();
         }
-        else {
-          newPos = randomizePosition();
-        }
+
         this.roundCount++;
         this.historyP0.add(newPos);
         this.historyP1.add(player1LastMove);
         this.historyP2.add(player2LastMove);
-
+        
         return newPos;
     }
 }
